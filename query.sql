51e3c59c5ee71b148c939d64f694eaa977ce3f60
Tharles Amaro <tharlesamaro@gmail.com>
2020-06-29 12:53:42 -0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,436 @@
+/* Eventos iniciais / de tabela */
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-1000',
+'I',
+'Informações do Empregador Contribuinte Órgão Público' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-1005',
+'I',
+'Tabela de Estabelecimentos Obras ou Unidades de Órgãos Públicos' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-1010',
+'I',
+'Tabela de Rubricas');
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1020',
+'I',
+'Tabela de Lotações Tributárias' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1030',
+'I',
+'Tabela de Cargos Empregos Públicos' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1035',
+'I',
+'Tabela de Carreiras Públicas' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1040',
+'I',
+'Tabela de Funções Cargos em Comissão' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1050',
+'I',
+'Tabela de Horários Turnos de Trabalho' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1060',
+'I',
+'Tabela de Ambientes de Trabalho' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1070',
+'I',
+'Tabela de Processos Administrativos Judiciais' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1080',
+'I',
+'Tabela de Operadores Portuários' );
+
+/* Eventos periódicos */
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1200',
+'P',
+'Remuneração de trabalhador vinculado ao Regime Geral de Previd Social' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1202',
+'P',
+'Remuneração de servidor vinculado a Regime Próprio de Previd Social' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1207',
+'P',
+'Benefícios previdenciários RPPS' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1210',
+'P',
+'Pagamentos de Rendimentos do Trabalho' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1250',
+'P',
+'Aquisição de Produção Rural' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1260',
+'P',
+'Comercialização da Produção Rural Pessoa Física' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1270',
+'P',
+'Contratação de Trabalhadores Avulsos Não Portuários' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1280',
+'P',
+'Informações Complementares aos Eventos Periódicos' );
+
+INSERT
+	INTO
+	tabela ( codigo,
+	grupo,
+	descricao )
+VALUES ( 'S-1295',
+'P',
+'Solicitação de Totalização para Pagamento em Contingência' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ( 'S-1298',
+'P',
+'Reabertura dos Eventos Periódicos' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-1299',
+'P',
+'Fechamento dos Eventos Periódicos' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-1300',
+'P',
+'Contribuição Sindical Patronal');
+
+/* Eventos não periódicos */
+
+NSERT
+INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2190',
+'N',
+'Admissão de Trabalhador Registro Preliminar' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2200',
+'N',
+'Cadastramento Inicial do Vínculo e Admissão Ingresso de Trabalhador');
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2205',
+'N',
+'Alteração de Dados Cadastrais do Trabalhador' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2206',
+'N',
+'Alteração de Contrato de Trabalho' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2210',
+'N',
+'Comunicação de Acidente de Trabalho' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2220',
+'N',
+'Monitoramento da Saúde do Trabalhador' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2221 ',
+'N',
+'Exame Toxicológico do Motorista Profissional' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2230 ',
+'N',
+'Afastamento Temporário');
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2240',
+'N',
+'Condições Ambientais do Trabalho Fatores de Risco' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2245',
+'N',
+'Treinamentos Capacitações Exercícios Simulados e Outras Anotações');
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao)
+VALUES ('S-2250',
+'N',
+'Aviso Prévio' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-2260',
+'N',
+'Convocação para Trabalho Intermitente' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-2298',
+'N',
+'Reintegração' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-2299 ',
+'N',
+'Desligamento' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-2300',
+'N',
+'Trabalhador Sem Vínculo de Emprego Estatutário Início' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-2306 ',
+'N',
+'Trabalhador Sem Vínculo de Emprego Estatutário Alteração Contratual' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-2399',
+'N',
+' Trabalhador Sem Vínculo de Emprego Estatutário Término' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-2400 ',
+'N',
+'Cadastro de Benefícios Previdenciários RPPS' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-3000',
+'N',
+' Exclusão de eventos' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-5001',
+'N',
+'Informações das contribuições sociais por trabalhador' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-5002 ',
+'N',
+'Imposto de Renda Retido na Fonte' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-5003',
+'N',
+'Informações do FGTS por trabalhador' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-5011',
+'N',
+'Informações das contribuições sociais consolidadas por contribuinte' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-5012',
+'N',
+'Informações do IRRF consolidadas por contribuinte' );
+
+INSERT
+	INTO
+	tabela (codigo,
+	grupo,
+	descricao )
+VALUES ('S-5013',
+'N',
